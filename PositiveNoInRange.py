be6e680d9c67caa8bd30996c3f90b6273a5408d4
Karan Shah <karanshah21241@gmail.com>
2020-07-16 17:31:38 +0530

    Created on 16/07/2020
    A python program to find positive numbers in a given range of numbers.
    Example :
    
    Input list1: [12, -7, 5, 64, -14]
    Output: [12, 5, 64]
    
    Input list2 = [12, 14, -95, 3]
    Output: [12, 14, 3]

---
+++ PositiveNoInRange.py
@@ -0,0 +1,10 @@
+lst = []
+
+n = int(input("Enter number of elements : "))
+
+print("Input: list1 = ")
+for i in range(0, n):
+    ele = int(input())
+    if ele >= 0:
+        lst.append(ele)
+print("Output: ", lst)
